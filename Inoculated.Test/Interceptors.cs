bed6c50ddf3373f39f0c253bd6979ba6cf90ee8e
Ayman Bouchareb <Ayman.bouchareb@outlook.fr>
2023-02-13 01:23:56 +0100

    Add General Purpose MethodHandler Prototype

--- Inoculated.Test/Interceptors.cs
+++ Inoculated.Test/Interceptors.cs
@@ -57,89 +57,14 @@ public class MemoizeAttribute : RewriterAttribute
         }
         return result.GetHashCode();
     }
-    public MethodData Invoke(MethodData method) {
-        (object Instance, object[] Parameters, object ReturnValue) = (null, null, null);
-        if(method.IsStatic) {
-            Parameters = method.Parameters.Select(p => p.Value).ToArray();
-        } else {
-            Instance = method.Parameters[0].Value;
-            Parameters = method.Parameters.Skip(1).Select(p => p.Value).ToArray();
-        }
-        var methodInfo = method.ReflectionInfo;
-
-
-        var result = methodInfo.Invoke(Instance, Parameters);
-        ReturnValue = result;
-
-        
-        method.ReturnValue = new ParameterData(
-            value : ReturnValue,
-            type : ReturnValue.GetType()
-        );
-        return method;
-    }
-
-    public MethodData InvokeAsync(MethodData method) {
-        (object Instance, object[] Parameters, object ReturnValue) = (null, null, null);
-        if(method.IsStatic) {
-            Parameters = method.Parameters.Select(p => p.Value).ToArray();
-        } else {
-            Instance = method.Parameters[0].Value;
-            Parameters = method.Parameters.Skip(1).Select(p => p.Value).ToArray();
-        }
-        var methodInfo = method.ReflectionInfo;
-
-
-        var result = (Task)methodInfo.Invoke(Instance, Parameters);
-        result.Wait();
-        var resultProperty = result.GetType().GetProperty("Result");
-        var resultValue = resultProperty.GetValue(result);
-        ReturnValue = resultValue;
-        
-        method.ReturnValue = new ParameterData(
-            value : resultValue,
-            type : resultValue.GetType()
-        );
-        return method;
-    }
-
-    private (IEnumerable, IEnumerator)  enumHandler;
-    public MethodData InvokeEnum(MethodData method) {
-        (object Instance, object[] Parameters, object ReturnValue) = (null, null, null);
-        if(method.IsStatic) {
-            Parameters = method.Parameters.Select(p => p.Value).ToArray();
-        } else {
-            Instance = method.Parameters[0].Value;
-            Parameters = method.Parameters.Skip(1).Select(p => p.Value).ToArray();
-        }
-        var methodInfo = method.ReflectionInfo;
-        enumHandler.Item1 = (IEnumerable)methodInfo.Invoke(Instance, Parameters);
-        enumHandler.Item2 = enumHandler.Item2 ?? enumHandler.Item1.GetEnumerator();
-        method.Stop = !enumHandler.Item2.MoveNext();
-        var resultValue = enumHandler.Item2.Current;
-        method.ReturnValue = new ParameterData(
-            value : resultValue,
-            type : resultValue.GetType()
-        );
-        return method;
-    }
+    Engine<Program.Entry> engine = new();
     public override MethodData OnCall(MethodData method)
     {
         var argumentsHash = StringifyAndHash(method.Parameters);
         if(method.MethodBehaviour is not MethodData.MethodType.Iter && cache.ContainsKey(argumentsHash)) {
             method.ReturnValue = cache[argumentsHash];
         } else {
-            switch(method.MethodBehaviour) {
-                case MethodData.MethodType.Async:
-                    method = InvokeAsync(method);
-                    break;
-                case MethodData.MethodType.Sync:
-                    method = Invoke(method);
-                    break;
-                case MethodData.MethodType.Iter:
-                    method = InvokeEnum(method);
-                    break;
-            }
+            method = engine.Invoke(method);
         }
         cache.TryAdd(argumentsHash, method.ReturnValue);
         Console.WriteLine(method);
